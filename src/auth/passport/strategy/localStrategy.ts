0403d2b1f00d7e0d2818c7682a0b2efa3dc979c9
hyun0509-iva <skyblue5030@gmail.com>
2024-10-24 16:44:34 +0900

    chore: 로그인시 유저 정보 수정

--- src/auth/passport/strategy/localStrategy.ts
+++ src/auth/passport/strategy/localStrategy.ts
@@ -5,6 +5,7 @@ import {
   Strategy as LocalStrategy
 } from "passport-local";
 import UsersModel from "../../../models/users";
+import { UsersSeivce } from "../../../services/users";
 
 const passportConfig: IStrategyOptionsWithRequest = {
   usernameField: "email",
@@ -16,29 +17,29 @@ const passportConfig: IStrategyOptionsWithRequest = {
 export default () => {
   passport.use(
     "local",
-    new LocalStrategy(passportConfig, async (req, email, password, done) => {
-      try {
-        // req 객체를 통해 아래와 같이 사용 가능
-        // console.log('Client IP 주소:', req.ip);
-        // console.log('HTTP 헤더 정보:', req.headers);
-        // console.log('요청 바디의 추가 필드:', req.body.rememberMe);
+    new LocalStrategy(
+      passportConfig,
+      async (req, req_email, req_password, done) => {
+        try {
+          /* 인증 처리 */
+          const userData = await UsersModel.findOne({ email: req_email });
+          if (!userData) {
+            return done(null, false, { message: "유저가 존재하지 않습니다." });
+          }
+          const { password, ...user } = userData._doc;
 
-        const user = await UsersModel.findOne({ email });
-
-        if (!user) {
-          return done(null, false, { message: "유저가 존재하지 않습니다." }); //인증 실패할 경우 default: 401 Unauthorized statusCode
+          const isMatch = await bcrypt.compare(req_password, password);
+          if (!isMatch) {
+            return done(null, false, {
+              message: "비밀번호가 일치하지 않습니다."
+            });
+          }
+          
+          return done(null, user);
+        } catch (error) {
+          return done(error);
         }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-          return done(null, false, {
-            message: "비밀번호가 일치하지 않습니다."
-          });
-        }
-        return done(null, user);
-      } catch (error) {
-        return done(error);
       }
-    })
+    )
   );
 };
