0ebe2a9479e1bd3e6d063cdac1e960649952f4db
hyun0509-iva <skyblue5030@gmail.com>
2024-11-05 13:34:14 +0900

    chore: 에러 핸들러 메시지 수정

--- src/exceptions/NotFoundException.ts
+++ src/exceptions/NotFoundException.ts
@@ -2,7 +2,7 @@ import HttpException from "./HttpException";
 
 class NotFoundException extends HttpException {
   constructor() {
-    super(404, "요청하신 페이지를 찾을 수 없습니다.");
+    super(404, "요청하신 api를 찾을 수 없습니다.");
   }
 }
 
